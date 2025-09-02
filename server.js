bc0ff577196ce8c328003ed48c44ec181034f9ef
Hemanthvinnu <hemanthkatagoni@gmail.com>
2025-09-03 02:10:03 +1000

    sockets

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const http = require('http');            // ⬅ add
-const { Server } = require('socket.io'); // ⬅ add
+const http = require('http');           
+const { Server } = require('socket.io'); 
 
 const app = express();
 const port = process.env.PORT || 3001;
